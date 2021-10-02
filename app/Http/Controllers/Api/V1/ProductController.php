e7bd0dfbf8113944ce05d65d89ab296bfc7e4ac5
joshuaprima <joshuaprimap@gmail.com>
2021-10-03 00:45:19 +0700

    enhance api product

--- app/Http/Controllers/Api/V1/ProductController.php
+++ app/Http/Controllers/Api/V1/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Services\Product\ProductCommands;
 use App\Http\Services\Product\ProductQueries;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProductController extends Controller
 {
@@ -22,11 +23,13 @@ class ProductController extends Controller
 
     //Create Produk
     public function createProduct(Request $request){
+        $request['full_name'] = Auth::user()->full_name;
         return $this->productCommands->createProduct($request);
     }
 
     //Update Produk
     public function updateProduct($product_id, $merchant_id, Request $request){
+        $request['full_name'] = Auth::user()->full_name;
         return $this->productCommands->updateProduct($product_id, $merchant_id, $request);
     }
 
@@ -52,6 +55,7 @@ class ProductController extends Controller
 
     //Adjust Stok Produk
     public function updateStockProduct($product_id, $merchant_id, Request $request){
+        $request['full_name'] = Auth::user()->full_name;
         return $this->productCommands->updateStockProduct($product_id, $merchant_id, $request);
     }
 }
