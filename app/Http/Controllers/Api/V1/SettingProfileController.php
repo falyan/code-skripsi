685e30e3d0d16c9162b6f8511a3543ba969961a7
rizalihwan <rizalihwan94@gmail.com>
2021-10-05 16:08:15 +0700

    takeout middleware auth get profile buyer

--- app/Http/Controllers/Api/V1/SettingProfileController.php
+++ app/Http/Controllers/Api/V1/SettingProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Services\Profile\ProfileCommands;
 use App\Http\Services\Profile\ProfileQueries;
+use Illuminate\Support\Facades\Validator;
+use Exception;
 
 class SettingProfileController extends Controller
 {
@@ -16,7 +18,15 @@ class SettingProfileController extends Controller
 
     public function index()
     {
+        $validator = Validator::make(request()->all(), [
+            'related_pln_mobile_customer_id' => 'nullable|exists:cart,related_pln_mobile_customer_id'
+        ]);
+
         try {
+            if ($validator->fails()) {
+                throw new Exception($validator->errors(), 400);
+            }
+
             return $this->respondWithData([
                 'user' => $this->profileQueries->getUser(),
                 'toko' => $this->profileQueries->getMerchant()
