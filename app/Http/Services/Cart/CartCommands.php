3b3fb2658f27142e5ee6913fbc322d804b76bd35
Ariq Jusuf <ariq2901@gmail.com>
2021-10-05 11:25:46 +0700

    profile toko

--- app/Http/Services/Cart/CartCommands.php
+++ app/Http/Services/Cart/CartCommands.php
@@ -11,8 +11,5 @@ use Illuminate\Support\Facades\DB;
 
 class CartCommands{
     static $error_codes = [400, 401, 402, 403, 404, 405, 406, 407, 408, 409, 410, 411, 412, 413, 414, 415, 416, 417, 418, 421, 422, 423, 424, 425, 426, 428, 429, 431, 451, 500, 501, 502, 503, 504, 505, 506, 507, 508, 510, 511];
-
-    public static function aturToko($request, $merchant_id){
-    }
 }
 ;
