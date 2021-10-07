a7b2af6270aafba6295c145e2a651679f6edb6c0
joshuaprima <joshuaprimap@gmail.com>
2021-10-07 11:08:04 +0700

    enhance api cart detail

--- app/Http/Services/Cart/CartQueries.php
+++ app/Http/Services/Cart/CartQueries.php
@@ -23,7 +23,7 @@ class CartQueries{
         if ($buyer_id != null){
             $cart = Cart::with(['cart_detail' => function($product)
             {$product->with(['product' => function($product_detail)
-            {$product_detail->with(['product_stock', 'product_photo']);}]);}])
+            {$product_detail->with(['product_stock', 'product_photo', 'merchant']);}]);}])
                 ->where('buyer_id', $buyer_id)->get();
 
 //            if ($cart->isEmpty()){
@@ -39,7 +39,7 @@ class CartQueries{
         }else{
             $cart = Cart::with(['cart_detail' => function($product)
             {$product->with(['product' => function($product_detail)
-            {$product_detail->with(['product_stock', 'product_photo']);}]);}])
+            {$product_detail->with(['product_stock', 'product_photo', 'merchant']);}]);}])
                 ->where('related_pln_mobile_customer_id', $related_id)->get();
 
 //            if ($cart->isEmpty()){
