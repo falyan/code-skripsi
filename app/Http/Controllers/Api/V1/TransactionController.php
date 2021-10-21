a1887ccf864b25f760a14fd2aac5cad502ced37f
joshuaprima <joshuaprimap@gmail.com>
2021-10-21 14:37:27 +0700

    enhance api accept order become multiple

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -454,10 +454,39 @@ class TransactionController extends Controller
         $error;
     }
 
-    public function acceptOrder($order_id)
+    public function acceptOrder(Request $request)
     {
         try {
-            return $this->transactionCommand->updateOrderStatus($order_id, '02');
+            $rules = [
+                'id.*' => 'required',
+            ];
+
+            $validator = Validator::make($request->all(), $rules, [
+                'required' => ':attribute diperlukan.',
+            ]);
+
+            if ($validator->fails()) {
+                $errors = collect();
+                foreach ($validator->errors()->getMessages() as $key => $value) {
+                    foreach ($value as $error) {
+                        $errors->push($error);
+                    }
+                }
+
+                return $this->respondValidationError($errors, 'Validation Error!');
+            }
+
+            foreach ($request->id as $order_id){
+                $response = $this->transactionCommand->updateOrderStatus($order_id, '02');
+                if ($response['success'] == false){
+                    return $response;
+                }
+            }
+
+            $response['success'] = true;
+            $response['message'] = 'Berhasil merubah status pesanan';
+            return $response;
+
         } catch (Exception $e) {
             return $this->respondErrorException($e, request());
         }
--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,7 @@ $router->group(['prefix' => 'v1', 'namespace' => 'Api\V1'], function () use ($ro
                     $router->post('/{id}/approve', 'TransactionController@approveOrder');
                     $router->post('/{id}/reject', 'TransactionController@rejectOrder');
                     $router->post('/{id}/deliver', 'TransactionController@deliverOrder');
-                    $router->post('accept/{order_id}', 'TransactionController@acceptOrder');
+                    $router->post('accept', 'TransactionController@acceptOrder');
                     $router->post('reject/{order_id}', 'TransactionController@rejectOrder');
                     $router->post('awb-number/{order_id}/{awb}', 'TransactionController@addAwbNumberOrder');
                 });
