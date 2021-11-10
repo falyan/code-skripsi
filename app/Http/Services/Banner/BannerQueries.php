11d666ddddb0d88cad5fb73c9322643197efc6e0
Hasby <hasbiyallah.syafruddin@gmail.com>
2021-11-10 23:21:13 +0700

    pindah lokasi init banner url

--- app/Http/Services/Banner/BannerQueries.php
+++ app/Http/Services/Banner/BannerQueries.php
@@ -6,21 +6,13 @@ use App\Http\Services\Service;
 
 class BannerQueries extends Service
 {
-    static $banner_url;
-
-    static function init()
-    {
-        self::$banner_url = config('credentials.banner.url_flash_popup');
-    }
-
     public function getFlashPopup()
     {
-        $result = self::$banner_url ?? '';
-        
+        $result = static::$banner_url;
         return [
             'sukses' => true,
             'message' => !empty($result) ? 'Berhasil mendapatkan data banner' : 'Data banner tidak ada',
-            'data' => ['url_path' => $result]
+            'data' => ['url_path' => $result ?? '']
         ];
     }
 }
