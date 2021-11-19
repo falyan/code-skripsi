addc8c90c61f50adcd849e2fd5d8f00ecd2374b3
Hasby <hasbiyallah.syafruddin@gmail.com>
2021-11-19 16:37:38 +0700

    tambah logs ke channel stderr

--- config/logging.php
+++ config/logging.php
@@ -39,7 +39,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['daily', 'stderr'],
         ],
 
         'single' => [
