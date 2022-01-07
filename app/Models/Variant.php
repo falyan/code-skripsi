3de09ec05a8b7c37590d4e49edf0f561b76980f1
Dhani <mramdhani010@gmail.com>
2022-01-07 13:00:19 +0700

    softdelete

--- app/Models/Variant.php
+++ app/Models/Variant.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Variant extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $table = 'variant';
     protected $guarded = [];
 
--- app/Models/VariantStock.php
+++ app/Models/VariantStock.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VariantStock extends Model
 {
+    use HasFactory, SoftDeletes;
+
     protected $table = 'variant_stock';
     protected $guarded = [];
 
