5ca6e1a19423736cc58704997e937500659e47b7
Dhani <mramdhani010@gmail.com>
2022-01-09 22:36:55 +0700

    command create variant value

---
+++ app/Http/Services/Variant/VariantCommands.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Services\Variant;
+
+use App\Models\Product;
+use App\Models\Variant;
+use App\Models\VariantValue;
+use Exception;
+use Illuminate\Support\Facades\DB;
+
+class VariantCommands
+{
+    public function createVariantValue($product_id, $data)
+    {
+        try {
+            if (!isset($data['variant_id']) || !isset($data['nama'])) {
+                return [];
+            }
+
+            DB::beginTransaction();
+
+            $variant_value = VariantValue::create([
+                'variant_id' => $data['variant_id'],
+                'product_id' => $product_id,
+                'nama' => $data['nama'],
+            ]);
+
+            DB::commit();
+
+            return $variant_value;
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw new Exception($e->getMessage(), $e->getCode());
+        }
+    }
+}
