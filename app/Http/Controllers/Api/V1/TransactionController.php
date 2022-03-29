8a225e08724f2f6e08fdb022bfc7d8f1e55f2c2e
joshuaprima <joshuaprimap@gmail.com>
2022-03-29 14:17:14 +0700

    enhance delay api biller notify

--- app/Http/Controllers/Api/V1/TransactionController.php
+++ app/Http/Controllers/Api/V1/TransactionController.php
@@ -1002,7 +1002,7 @@ class TransactionController extends Controller
             }
 
             $this->mailSenderManager->mailPaymentSuccess($order->id);
-            sleep(10);
+            sleep(70);
             return $response;
         } catch (Exception $e) {
             return $this->respondErrorException($e, request());
