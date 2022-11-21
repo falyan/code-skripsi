737feb082abb9515bb598581e83d68780f65e9a7
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-11-21 22:27:04 +0700

    new endpoint v2 profile merchant

--- routes/web.php
+++ routes/web.php
@@ -189,6 +189,7 @@ $router->group(['prefix' => 'v1', 'namespace' => 'Api\V1'], function () use ($ro
                 $router->get('/official-store', 'MerchantController@getOfficialStore');
                 $router->get('/official-store/search', 'MerchantController@searchOfficialStoreByName');
                 $router->get('{merchant_id}', 'MerchantController@publicProfile');
+                $router->get('v2/{merchant_id}', 'MerchantController@publicProfileV2');
                 $router->get('/official/{category_key}', 'MerchantController@getOfficialMerchant');
                 $router->get('/official/{category_key}/{sub_category_key}', 'MerchantController@getOfficialMerchantBySubCategory');
             });
