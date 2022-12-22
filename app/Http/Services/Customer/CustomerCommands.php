a340cd1ffd1d25765eca8f1e9b16ed89e008b791
Muhammad Ramdhani <mramdhani010@gmail.com>
2022-12-22 14:40:35 +0700

    fixed bug default address when create & update address

--- app/Http/Services/Customer/CustomerCommands.php
+++ app/Http/Services/Customer/CustomerCommands.php
@@ -28,20 +28,21 @@ class CustomerCommands
         $customer_address->title = $data['title'];
         $customer_address->is_default = $data['is_default'] ?? false;
 
-        // if ($data['is_default'] == true) {
-        //     $change_isdefault = $this->changeIsDefaultToFalse($customer_id);
-        //     if ($change_isdefault == false) {
-        //         $response['success'] = false;
-        //         $response['message'] = 'Gagal merubah alamat utama';
-        //         return $response;
-        //     }
-        // }
-
         if (!$customer_address->save()) {
             $response['success'] = false;
             $response['message'] = 'Gagal menyimpan alamat customer';
             return $response;
         }
+
+        if ($customer_address->is_default == true) {
+            $change_isdefault = $this->changeIsDefaultToFalse($customer_id, $customer_address->id);
+            if ($change_isdefault == false) {
+                $response['success'] = false;
+                $response['message'] = 'Gagal merubah alamat utama';
+                return $response;
+            }
+        }
+
         $response['success'] = true;
         $response['message'] = 'Berhasil menyimpan alamat customer';
         return $response;
@@ -60,15 +61,16 @@ class CustomerCommands
         $customer_address->receiver_name = $data['receiver_name'] == null ? ($customer_address->receiver_name) : ($data['receiver_name']);
         $customer_address->receiver_phone = $data['receiver_phone'] == null ? ($customer_address->receiver_phone) : ($data['receiver_phone']);
         $customer_address->title = $data['title'] == null ? ($customer_address->title) : ($data['title']);
-        $customer_address->is_default = $data['is_default'] == null ? ($customer_address->is_default) : ($data['is_default']);
-        // if ($data['is_default'] == true) {
-        //     $change_isdefault = $this->changeIsDefaultToFalse($customer_id);
-        //     if ($change_isdefault == false) {
-        //         $response['success'] = false;
-        //         $response['message'] = 'Gagal merubah alamat utama';
-        //         return $response;
-        //     }
-        // }
+        $customer_address->is_default = $data['is_default'] ?? false;
+
+        if ($data['is_default'] == true) {
+            $change_isdefault = $this->changeIsDefaultToFalse($customer_id, $id);
+            if ($change_isdefault == false) {
+                $response['success'] = false;
+                $response['message'] = 'Gagal merubah alamat utama';
+                return $response;
+            }
+        }
 
         if (!$customer_address->save()) {
             $response['success'] = false;
@@ -103,15 +105,18 @@ class CustomerCommands
         return $response;
     }
 
-    private function changeIsDefaultToFalse($customer_id)
+    private function changeIsDefaultToFalse($customer_id, $customer_address_id)
     {
         $data = $this->customerQueries->getDefaultCustomerAddress($customer_id);
         if ($data == null) {
             return true;
         }
-        $data->is_default = false;
-        if (!$data->save()) {
-            return false;
+
+        if ($data->id != $customer_address_id) {
+            $data->is_default = false;
+            if (!$data->save()) {
+                return false;
+            }
         }
         return true;
     }
