a5aca33bec9bc54383ad70930dc975bc72033909
naufalatha <naufalatha130@gmail.com>
2023-09-20 06:18:11 +0700

    feat (api) : Fixing kalkulasi simulasi cicilan bni installment

--- app/Http/Services/Installment/InstallmentQueries.php
+++ app/Http/Services/Installment/InstallmentQueries.php
@@ -14,13 +14,31 @@ class InstallmentQueries extends Service
         }])->where('status', 1)->get();
 
         $providers = $providers->transform(function ($provider) use ($price) {
-            foreach ($provider->details as $detail) {
-                if (!empty($price)) {
-                    $markup_price = $price / (1 - ($detail->mdr_percentage / 100));
-                    $installment_price = ($markup_price / $detail->tenor) + ($markup_price * $detail->fee_percentage / 100);
-                    $detail->simulation_price = round($installment_price);
-                } else {
-                    $detail->installment_price = 0;
+            if ($provider->provider_code == 'BRI-CERIA') {
+                foreach ($provider->details as $detail) {
+                    if (!empty($price)) {
+                        $markup_price = $price / (1 - ($detail->mdr_percentage / 100));
+                        $installment_price = ($markup_price / $detail->tenor) + ($markup_price * $detail->interest_percentage / 100);
+                        $detail->simulation_price_installment = round($installment_price);
+                        $detail->simulation_fee_installment = round($markup_price * $detail->mdr_percentage / 100);
+                    } else {
+                        $detail->simulation_price_installment = 0;
+                        $detail->simulation_fee_installment = 0;
+                    }
+                }
+            } else if ($provider->provider_code == 'BNI-INSTALLMENT') {
+                foreach ($provider->details as $detail) {
+                    if (!empty($price)) {
+                        $percentages = ($detail->mdr_percentage + $detail->fee_percentage);
+                        $markup_price = $price / (1 - ($percentages / 100));
+                        $detail->simulation_price_installment = round($markup_price / $detail->tenor);
+                        $detail->simulation_fee_installment = round($markup_price * $percentages / 100);
+                        $detail->mdr_fee_percentage = $percentages;
+                    } else {
+                        $detail->simulation_price_installment = 0;
+                        $detail->simulation_fee_installment = 0;
+                        $detail->mdr_fee_percentage = 0;
+                    }
                 }
             }
 
