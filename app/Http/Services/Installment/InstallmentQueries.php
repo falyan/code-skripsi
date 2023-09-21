f16254cfb3364e39b803d9f1b95e475082ce2188
naufalatha <naufalatha130@gmail.com>
2023-09-21 19:44:36 +0700

    feat (api) : Fixing  get tenor by provide id

--- app/Http/Services/Installment/InstallmentQueries.php
+++ app/Http/Services/Installment/InstallmentQueries.php
@@ -17,10 +17,23 @@ class InstallmentQueries extends Service
             $providers = $providers->where('id', $provider_id);
         }
 
+        if (!empty($price)) {
+            $providers = $providers->where(function ($query) use ($price) {
+                $query->where(function ($query) use ($price) {
+                    $query->where('provider_code', 'bri-ceria')
+                        ->where('min_price', '<=', $price)
+                        ->where('max_price', '>=', $price);
+                })->orWhere(function ($query) use ($price) {
+                    $query->where('provider_code', 'bni-installment')
+                        ->where('min_price', '<=', $price);
+                });
+            });
+        }
+
         $providers = $providers->get();
 
         $providers = $providers->transform(function ($provider) use ($price) {
-            if ($provider->provider_code == 'BRI-CERIA') {
+            if ($provider->provider_code == 'bri-ceria') {
                 foreach ($provider->details as $detail) {
                     if (!empty($price)) {
                         $markup_price = $price / (1 - ($detail->mdr_percentage / 100));
@@ -32,7 +45,7 @@ class InstallmentQueries extends Service
                         $detail->simulation_fee_installment = 0;
                     }
                 }
-            } else if ($provider->provider_code == 'BNI-INSTALLMENT') {
+            } else if ($provider->provider_code == 'bni-installment') {
                 foreach ($provider->details as $detail) {
                     if (!empty($price)) {
                         $markup_price = ($price + $detail->fee_provider) / (1 - ($detail->mdr_percentage / 100));
@@ -68,21 +81,18 @@ class InstallmentQueries extends Service
         }])->where('status', 1)->where('id', $provider_id)->get();
 
         $providers = $providers->transform(function ($provider) use ($price) {
-            if ($provider->provider_code == 'BRI-CERIA') {
+            if ($provider->provider_code == 'bri-ceria') {
                 unset($provider->details);
                 $provider->details = [];
-            } else if ($provider->provider_code == 'BNI-INSTALLMENT') {
+            } else if ($provider->provider_code == 'bni-installment') {
                 foreach ($provider->details as $detail) {
                     if (!empty($price)) {
-                        $percentages = ($detail->mdr_percentage + $detail->fee_percentage);
-                        $markup_price = $price / (1 - ($percentages / 100));
+                        $markup_price = ($price + $detail->fee_provider) / (1 - ($detail->mdr_percentage / 100));
                         $detail->simulation_price_installment = round($markup_price / $detail->tenor);
-                        $detail->simulation_fee_installment = round($markup_price * $percentages / 100);
-                        $detail->mdr_fee_percentage = $percentages;
+                        $detail->simulation_fee_installment = round($markup_price * $detail->mdr_percentage / 100);
                     } else {
                         $detail->simulation_price_installment = 0;
                         $detail->simulation_fee_installment = 0;
-                        $detail->mdr_fee_percentage = 0;
                     }
                 }
             }
@@ -106,10 +116,10 @@ class InstallmentQueries extends Service
             }
         }])->where('id', $providerId)->where('status', 1)->first();
 
-        if ($providers->provider_code === 'BRI-CERIA') {
+        if ($providers->provider_code === 'bri-ceria') {
             $markup_price = $price / (1 - ($providers->details[0]->mdr_percentage / 100));
             $installment_fee = round($markup_price * $providers->details[0]->mdr_percentage / 100);
-        } else if ($providers->provider_code === 'BNI-INSTALLMENT') {
+        } else if ($providers->provider_code === 'bni-installment') {
             $markup_price = ($price + $providers->details[0]->fee_provider) / (1 - ($providers->details[0]->mdr_percentage / 100));
             $installment_price = round($markup_price / $tenor);
             $installment_fee = round($markup_price * $providers->details[0]->mdr_percentage / 100);
