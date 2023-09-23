c07e5e32565d3c62fd90685b81a2bcbaf2681f0d
naufalatha <naufalatha130@gmail.com>
2023-09-23 10:48:02 +0700

    feat (api) : Fixing validasi exception installment order when status order 00

--- app/Http/Services/Transaction/TransactionQueries.php
+++ app/Http/Services/Transaction/TransactionQueries.php
@@ -291,7 +291,7 @@ class TransactionQueries extends Service
                 $product->with(['product' => function ($j) {
                     $j->with('ev_subsidy');
                 }, 'variant_value_product']);
-            }, 'progress', 'merchant', 'delivery' => function ($delivery) {
+            }, 'progress', 'progress_active', 'complaint', 'merchant', 'delivery' => function ($delivery) {
                 $delivery->with(['subdistrict', 'district', 'city', 'city.province']);
             }, 'buyer', 'ev_subsidy', 'payment', 'review' => function ($review) {
                 $review->with(['review_photo']);
@@ -303,9 +303,11 @@ class TransactionQueries extends Service
         ])->find($id);
 
         if (Auth::user()->type === 'buyer') {
-            if (empty($has_installment)) {
-                if ($data->installment != null) {
-                    throw new Exception('Untuk melihat detail transaksi dengan cicilan, silahkan update aplikasi Anda terlebih dahulu');
+            if ($data->progress_active->status_code == '00') {
+                if (empty($has_installment)) {
+                    if ($data->installment != null) {
+                        throw new Exception('Untuk melihat detail transaksi dan melanjutkan pembayaran dengan cicilan, silahkan update aplikasi Anda terlebih dahulu');
+                    }
                 }
             }
         }
