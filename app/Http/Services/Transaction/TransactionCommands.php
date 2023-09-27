ffc16fb0a625c90be704f10d20c8fa945c7ccefb
naufalatha <naufalatha130@gmail.com>
2023-09-27 11:06:31 +0700

    fix (api) : formatting installment tenor when push order BA additional info

--- app/Http/Services/Transaction/TransactionCommands.php
+++ app/Http/Services/Transaction/TransactionCommands.php
@@ -1661,6 +1661,10 @@ class TransactionCommands extends Service
                 }
             }
 
+            if (isset($datas['installment_tenor'])) {
+                $installment_tenor = $datas['installment_tenor'] < 10 ? str_pad($datas['installment_tenor'], 2, '0', STR_PAD_LEFT) : $datas['installment_tenor'];
+            }
+
             $url = sprintf('%s/%s', static::$apiendpoint, 'booking');
             $body = [
                 'no_reference' => $no_reference,
@@ -1675,7 +1679,7 @@ class TransactionCommands extends Service
                 'phone_number' => $customer->phone,
                 'expired_invoice' => $exp_date,
                 'additional_info7' => isset($datas['installment_provider_fee']) ? $datas['installment_provider_fee'] : null,
-                'additional_info8' => isset($datas['installment_tenor']) ? $datas['installment_tenor'] : null,
+                'additional_info8' => isset($installment_tenor) ? $installment_tenor : null,
                 'additional_info9' => isset($datas['installment_actual_price']) ? $datas['installment_actual_price'] : null,
                 'additional_info10' => isset($datas['installment_fee']) ? $datas['installment_fee'] : null,
             ];
