8957d57e30cd69f969c2cea88762b27759fa7ba2
baguspanji <pituns060@gmail.com>
2023-10-05 16:57:52 +0700

    update log icp

--- app/Http/Services/Transaction/TransactionCommands.php
+++ app/Http/Services/Transaction/TransactionCommands.php
@@ -240,7 +240,12 @@ class TransactionCommands extends Service
 
             $response = json_decode($response->getBody());
 
-            LogService::setUrl($url)->setRequest($body)->setResponse($response)->setServiceCode('iconpay')->setCategory('out')->log();
+            Log::info("E00002", [
+                'path_url' => "iconpay.booking",
+                'query' => [],
+                'body' => $body,
+                'response' => $response,
+            ]);
 
             throw_if(!$response, Exception::class, new Exception('Terjadi kesalahan: Data tidak dapat diperoleh', 500));
 
@@ -937,7 +942,12 @@ class TransactionCommands extends Service
 
             $response = json_decode($response->getBody());
 
-            LogService::setUrl($url)->setRequest($body)->setResponse($response)->setServiceCode('iconpay')->setCategory('out')->log();
+            Log::info("E00002", [
+                'path_url' => "iconpay.booking",
+                'query' => [],
+                'body' => $body,
+                'response' => $response,
+            ]);
 
             throw_if(!$response, Exception::class, new Exception('Terjadi kesalahan: Data tidak dapat diperoleh', 500));
 
@@ -1737,7 +1747,7 @@ class TransactionCommands extends Service
             $response = json_decode($response->getBody());
 
             Log::info("E00002", [
-                'path_url' => "ba.booking",
+                'path_url' => "iconpay.booking",
                 'query' => [],
                 'body' => $body,
                 'response' => $response,
