281f5f7d1d4fb690d2b01d63c919be2412d0b235
naufalatha <naufalatha130@gmail.com>
2023-10-06 15:41:12 +0700

    Fix (api) : withdrawal inquiry without disbursement

--- app/Http/Controllers/Api/V1/IconcashController.php
+++ app/Http/Controllers/Api/V1/IconcashController.php
@@ -243,18 +243,19 @@ class IconcashController extends Controller
             $response = IconcashInquiry::createWithdrawalInquiry($iconcash, $bank_account_name, $bank_account_no, $bank_id, $nominal, $source_account_id);
 
             return $this->respondWithData([
-                'order_id' => $response->orderId,
-                'invoice_id' => $response->invoiceId,
-                'source_account_id' => $response->sourceAccountId,
-                'source_account_name' => $response->sourceAccountName,
-                'nominal' => $response->nominal,
-                'fee' => $response->fee,
-                'total' => $response->total,
-                'bank_id' => $response->bankId,
-                'bank_code' => $response->bankCode,
-                'bank_name' => $response->bankName,
-                'bank_account_no' => $response->bankAccountNo,
-                'bank_account_name' => $response->bankAccountName,
+                'order_id' => data_get($response, 'orderId') ?? '',
+                'invoice_id' => data_get($response, 'invoiceId') ?? '',
+                'source_account_id' => data_get($response, 'sourceAccountId') ?? '',
+                'source_account_name' => data_get($response, 'sourceAccountName') ?? '',
+                'nominal' => data_get($response, 'nominal') ?? '',
+                'fee' => data_get($response, 'fee') ?? '',
+                'admin_fee' => data_get($response, 'adminFee') ?? '',
+                'total' => data_get($response, 'total') ?? '',
+                'bank_id' => data_get($response, 'bankId') ?? '',
+                'bank_code' => data_get($response, 'bankCode') ?? '',
+                'bank_name' => data_get($response, 'bankName') ?? '',
+                'bank_account_no' => data_get($response, 'bankAccountNo') ?? '',
+                'bank_account_name' => data_get($response, 'bankAccountName') ?? '',
             ], 'Proses Inquiry Berhasil!');
         } catch (Exception $e) {
             return $this->respondErrorException($e, request());
