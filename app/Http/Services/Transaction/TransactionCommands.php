992a67998f86e78352749a889a3940e80093b564
baguspanji <pituns060@gmail.com>
2023-10-18 17:33:00 +0700

    update mdr bug

--- app/Http/Services/Transaction/TransactionCommands.php
+++ app/Http/Services/Transaction/TransactionCommands.php
@@ -1400,7 +1400,7 @@ class TransactionCommands extends Service
                         $promo_master->save();
                     }
 
-                    if (!$limit_merchant && $value_ongkir > 0) {
+                    if ($value_ongkir > 0) {
                         $promo_log = new PromoLog();
                         $promo_log->order_id = $order->id;
                         $promo_log->promo_master_id = $promo_merchant_ongkir['promo_master']['id'];
@@ -1410,8 +1410,6 @@ class TransactionCommands extends Service
                         $promo_log->value = $value_ongkir;
                         $promo_log->created_by = 'System';
                         $promo_log->save();
-                    } else {
-                        $value_ongkir = 0;
                     }
                 }
 
@@ -1529,7 +1527,7 @@ class TransactionCommands extends Service
                         $promo_master->save();
                     }
 
-                    if (!$limit_merchant && $value_flash_sale > 0) {
+                    if ($value_flash_sale > 0) {
                         $promo_log = new PromoLog();
                         $promo_log->order_id = $order->id;
                         $promo_log->promo_master_id = $promo_merchant_flash_sale['promo_master']['id'];
@@ -1539,8 +1537,6 @@ class TransactionCommands extends Service
                         $promo_log->value = $value_flash_sale;
                         $promo_log->created_by = 'System';
                         $promo_log->save();
-                    } else {
-                        $value_flash_sale = 0;
                     }
 
                     // sementara ketika flash sale nempel merchant
