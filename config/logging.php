405d0218fdc892374fb6415301797949db70b56f
baguspanji <pituns060@gmail.com>
2023-10-31 10:03:28 +0700

    add mail debug

--- config/logging.php
+++ config/logging.php
@@ -55,6 +55,13 @@ return [
             'days' => 30,
         ],
 
+        'mail_log' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/mail.log'),
+            'level' => 'debug',
+            'days' => 7,
+        ],
+
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
--- config/mail.php
+++ config/mail.php
@@ -71,7 +71,7 @@ return [
 
         'log' => [
             'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
+            'channel' => 'mail_log',
         ],
 
         'array' => [
