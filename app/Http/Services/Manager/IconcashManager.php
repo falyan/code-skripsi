9d6b33b9354a574e020cf4284f9cdeb696db0d8b
naufalatha <naufalatha130@gmail.com>
2023-12-04 17:54:06 +0700

    test A9api) : normal 500 iconcash

--- app/Http/Services/Manager/IconcashManager.php
+++ app/Http/Services/Manager/IconcashManager.php
@@ -233,7 +233,7 @@ class IconcashManager
     {
         $param = self::setParamAPI([]);
 
-        $url = sprintf('%s/%s', self::$apiendpoint, 'api/query/balance/cusmer' . $param);
+        $url = sprintf('%s/%s', self::$apiendpoint, 'api/query/balance/customer' . $param);
 
         $response = self::$curl->request('GET', $url, [
             'headers' => [
